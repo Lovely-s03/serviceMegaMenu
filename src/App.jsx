1cef4e00d651dc462269b8cf5f72f72127d5bfe5
Lovely-s03 <supriyalovely0306@gmail.com>
2025-01-21 17:45:33 +0530

    SeriveDropDown

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Service from './component/Service'
+
+const App = () => {
+  return (
+    <div>
+      <Service/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Service.jsx
@@ -0,0 +1,105 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Service = () => {
+  return (
+    <>
+      <div className='wrapper'>
+        <nav>
+          <ul>
+            <li className='dropdown'>
+              <a href="#">Services</a>
+              <div className='menu-area'>
+                <div className='menu-column'>
+                  <h4>Strategy & Design</h4>
+                  <ul>
+                  <li>
+      <span style={{ whiteSpace: 'nowrap' }}>Product Strategy</span>
+      <span style={{ display: 'block' }}>& Roadmap</span>
+    </li>
+
+                    <h4>Product Design</h4>
+                    <li>UI/UX Design</li>
+                    <hr />
+                  </ul>
+                  <h4>E-Commerce Development</h4>
+                  <ul>
+                    <li>BigCommerce</li>
+                    <li>Shopify</li>
+                    <li>Headless</li>
+                    <li>WooCommerce</li>
+                    <li>Custom E-Commerce</li>
+                    <li>Store Setup</li>
+                    <li>Platform Migration</li>
+                  </ul>
+                </div>
+                <div className='menu-column'>
+                  <h4>Custom Software Development</h4>
+                  <ul>
+                  <li>
+      <span style={{ whiteSpace: 'nowrap' }}>Outsource Product</span>
+      <span style={{ display: 'block' }}>Development</span>
+    </li>
+    <li> 
+    <span style={{ whiteSpace: 'nowrap' }}>Enterprise Software</span>
+      <span style={{ display: 'block' }}>Development</span>
+    </li>
+              
+            
+                    <li>SaaS Product Development</li>
+                    <li>CRM & ERP Solutions</li>
+                    <li>Integration & API</li>
+                    <li> 
+    <span style={{ whiteSpace: 'nowrap' }}>Legacy Application</span>
+      <span style={{ display: 'block' }}>Modernization</span>
+    </li>
+     
+                    <li>AI/ML</li>
+                  </ul>
+                  <hr />
+                  <h4>Application Development</h4>
+                  <ul>
+                    <li>Web Development</li>
+                    <li>Mobile App Development</li>
+                    <li>PWA (Progressive Web Apps)</li>
+                  </ul>
+                </div>
+                <div className='menu-column'>
+                  <h4>Performance Marketing</h4>
+                  <ul>
+                    <li>SEO</li>
+                    <li>Social Media Campaigns</li>
+                    <li>PPC Advertising</li>
+                    <li>Content Marketing</li>
+                    <li>Email Marketing Automation</li>
+                    <li> 
+    <span style={{ whiteSpace: 'nowrap' }}>Conversion Rate</span>
+      <span style={{ display: 'block' }}>Optimization</span>
+    </li>
+                
+                  </ul>
+                  <hr />
+                  <h4>Managed Support Services</h4>
+                  <ul>
+                  <li> 
+    <span style={{ whiteSpace: 'nowrap' }}>Application Managed </span>
+      <span style={{ display: 'block' }}> Service</span>
+    </li>
+    <li> 
+    <span style={{ whiteSpace: 'nowrap' }}>Infrastructure Managed </span>
+      <span style={{ display: 'block' }}> Service</span>
+    </li>
+                
+                
+                  </ul>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Service;
